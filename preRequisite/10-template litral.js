e1d323feca3033f5fb06f04b7f6370d40def1896
legendryflyer <tavish.anade@gmail.com>
2024-06-07 18:19:09 +0530

    added

---
+++ preRequisite/10-template litral.js
@@ -0,0 +1,7 @@
+// const name = "Max";
+// const age = 29;
+// console.log("My name is " + name + " and I am " + age + " years old.");
+
+const name = "Max";
+const age = 29;
+console.log(`My name is ${name} and I am ${age} years old.`);
