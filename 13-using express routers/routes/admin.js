3f18de95ec367ebcfc9f5391c6e095ad18fe5f5e
legendryflyer <tavish.anade@gmail.com>
2024-06-16 15:57:54 +0530

    day7

--- 13-using express routers/routes/admin.js
+++ 13-using express routers/routes/admin.js
@@ -2,14 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
-// /admin/add-product=>GET
 router.get("/add-product", (req, res, next) => {
   res.send(
-    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">add product</button></input></form>'
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   );
 });
 
-// /admin/product=>POST
 router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
