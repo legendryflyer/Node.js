fa7317d4f791f8e804cbceaac822901bfcf91796
legendryflyer <tavish.anade@gmail.com>
2024-07-02 20:36:46 +0530

    installing and implimenting pug

--- 15-sharing data across requests and users/routes/shop.js
+++ 15-sharing data across requests and users/routes/shop.js
@@ -7,9 +7,12 @@ const adminData = require("./admin");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("shop.js", adminData.products);
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
-});
+// router.get("/", (req, res, next) => {
+//   console.log("shop.js", adminData.products);
+//   res.sendFile(path.join(rootDir, "views", "shop.html"));
+// });
 
+router.get("/", (req, res, next) => {
+  res.render("shop");
+});
 module.exports = router;
