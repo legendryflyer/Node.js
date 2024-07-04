965f3d054c9f8ff4e57b8ca5679730cebb85f9c6
legendryflyer <tavish.anade@gmail.com>
2024-07-04 19:03:19 +0530

    day17

--- 15-sharing data across requests and users/app.js
+++ 15-sharing data across requests and users/app.js
@@ -2,11 +2,14 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", "views");
+app.engine("handlebars", expressHbs());
+
+app.set("view engine", "handlebars");
+app.set("views", "handlebars");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
